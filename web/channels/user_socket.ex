9519decc8cea94985b93a22d3809e746c2004e6d
mike <mike706574@gmail.com>
2017-03-02 18:23:47 -0600

    Fix for real.

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,8 +6,7 @@ defmodule Bookmarker.UserSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket,
-    timeout 45_000
-  end
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
